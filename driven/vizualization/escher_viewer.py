75983d54e37192190f4b702ef23b2f33f1285ef1
Joao Cardoso <joaca@biosustain.dtu.dk>
2015-08-18 13:12:43 +0200

    extract visualization to a new package

---
+++ driven/vizualization/escher_viewer.py
@@ -0,0 +1,38 @@
+# Copyright 2015 Novo Nordisk Foundation Center for Biosustainability, DTU.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from cameo.visualization.escher_ext import NotebookBuilder
+from IPython.display import display
+
+
+class EscherViewer(object):
+    def __init__(self, data_frame, map_name, color_scales, normalization_functions):
+        self.data_frame = data_frame
+        self.map_name = map_name
+        self.builder = None
+        self.color_scales = color_scales
+        self.normalization_functions = normalization_functions
+
+    def __call__(self, column):
+        reaction_data = dict(self.data_frame[column].apply(self.normalization_functions[column]))
+        reaction_scale = self.color_scales[column]
+        if self.builder is None:
+            self._init_builder(reaction_data, reaction_scale)
+        else:
+            self.builder.update(reaction_data=reaction_data, reaction_scale=reaction_scale)
+
+    def _init_builder(self, reaction_data, reaction_scale):
+        self.builder = NotebookBuilder(map_name=self.map_name,
+                                       reaction_data=reaction_data,
+                                       reaction_scale=reaction_scale)
+        display(self.builder.display_in_notebook())
