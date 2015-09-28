f2a22eb10808035b696f3fcb06afb7d77e743a44
Joao Cardoso <joaca@biosustain.dtu.dk>
2015-09-28 11:46:39 +0200

    skip imat test until properly implemented

--- tests/test_flux_analysis.py
+++ tests/test_flux_analysis.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from __future__ import print_function
 import unittest
 import os
 from driven.flux_analysis.transcriptomics import gimme, imat
@@ -43,16 +44,18 @@ class TranscriptomicsTestCase(unittest.TestCase):
 
         self.assertGreater(gimme_res_050.inconsistency_score, .0)
 
+    @unittest.skip("Implementation is wrong")
     def test_imat(self):
         model = self._blazier_model
 
         imat_res_025_075 = imat(model, self._blazier_expression, low_cutoff=0.25, high_cutoff=0.75, condition="Exp#2")
 
+        print(imat_res_025_075.data_frame)
         self.assertTrue(all([imat_res_025_075[r] == 0 for r in ["R1", "R2"]]))
         self.assertTrue(all([imat_res_025_075[r] != 0 for r in ["R3", "R4", "R5", "R6", "R7", "R8"]]))
 
         imat_res_050_075 = imat(model, self._blazier_expression, low_cutoff=0.50, high_cutoff=0.75, condition="Exp#2")
-
+        print(imat_res_050_075.data_frame)
         self.assertTrue(all([imat_res_050_075[r] == 0 for r in ["R1", "R2", "R3", "R4"]]))
         self.assertTrue(all([imat_res_050_075[r] != 0 for r in ["R5", "R6", "R8"]]))
 
@@ -81,15 +84,20 @@ class ThermodynamicsTestCase(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_(self):
+    def test_tmfa(self):
         pass
 
 
-class C13TestCase(unittest.TestCase):
+class FluxomicsTestCase(unittest.TestCase):
     def setUp(self):
         pass
 
+    @unittest.skip("Not implemented")
     def test_fba(self):
-        pass
+        raise
+
+
+class ResultTestCase(unittest.TestCase):
+    pass
 
 
