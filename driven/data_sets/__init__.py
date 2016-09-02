818f6a2d98bc98654b14125627c2ebd682a81f29
Joao Cardoso <joaca@biosustain.dtu.dk>
2016-09-02 13:07:10 +0200

    feat: import Expression profile from data_sets

--- driven/data_sets/__init__.py
+++ driven/data_sets/__init__.py
@@ -11,3 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+
+from driven.data_sets.expression_profile import ExpressionProfile
