558e7aaa9aacda8ce8528e1fe4f73397a8e993c4
Joao Cardoso <joaca@biosustain.dtu.dk>
2016-09-02 13:08:42 +0200

    fix: remove stuff implemented in cameo

--- driven/_cobra_ext.py
+++ driven/_cobra_ext.py
@@ -15,7 +15,7 @@
 from __future__ import absolute_import, print_function
 
 
-from cobra import Metabolite, Reaction
+from cobra import Reaction
 from sympy import Symbol
 from sympy.parsing.ast_parser import parse_expr
 
@@ -25,28 +25,5 @@ def __gene_to_expression__(reaction):
     rule = reaction._gene_reaction_rule.replace("and", "+").replace("or", "*")
     return parse_expr(rule, local_dict)
 
-
-def __str__metabolite__(self):
-    if self.formula is not None and len(self.formula.elements) > 0:
-        return "%s (%s)" % (self.name, self.formula)
-    else:
-        return self.name
-
-
-def _repr_html_metabolite_(self):
-    return """
-    <table>
-        <tbody>
-            <tr><td><b>Id</b></td><td>%s</td></tr>
-            <tr><td><b>Name</b></td><td>%s</td></tr>
-            <tr><td><b>Formula</b></td><td>%s/td></tr>
-        </tbody>
-    </table>
-    """ % (self.id, self.name, self.formula)
-
-
-Metabolite.__str__ = __str__metabolite__
-Metabolite.__repr__ = __str__metabolite__
-Metabolite._repr_html_ = _repr_html_metabolite_
 Reaction.gene_expression = __gene_to_expression__
 
